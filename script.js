d5e499911cf56fa359cf562b739749f57fa7722a
Sydney <113209756+sydneymeza@users.noreply.github.com>
2024-04-30 10:58:35 -0400

    feat: added pictures still need to add writing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const carouselItems = document.querySelectorAll(".carousel-item");
+const carouselItems = document.querySelectorAll(".carousel-item1");
 let currentIndex = 0;
 
 function showSlide(index) {
@@ -28,3 +28,68 @@ showSlide(currentIndex);
 // Set up event listeners for next and previous buttons
 document.getElementById("nextBtn").addEventListener("click", nextSlide);
 document.getElementById("prevBtn").addEventListener("click", previousSlide);
+
+const carouselItems2 = document.querySelectorAll(".carousel-item2");
+let currentIndex2 = 0;
+
+function showSlide2(index) {
+  // Hide all carousel items
+  carouselItems2.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  // Show the slide at the specified index
+  carouselItems2[index].style.display = "block";
+}
+
+function nextSlide2() {
+  currentIndex2 = (currentIndex2 + 1) % carouselItems2.length;
+  showSlide2(currentIndex2);
+}
+
+function previousSlide2() {
+  currentIndex2 =
+    (currentIndex2 - 1 + carouselItems2.length) % carouselItems2.length;
+  showSlide2(currentIndex2);
+}
+
+// Show the first slide initially
+showSlide2(currentIndex2);
+
+// Set up event listeners for next and previous buttons
+document.getElementById("nextBtn2").addEventListener("click", nextSlide2);
+document.getElementById("prevBtn2").addEventListener("click", previousSlide2);
+
+
+
+const carouselItems3 = document.querySelectorAll(".carousel-item3");
+let currentIndex3 = 0;
+
+function showSlide3(index) {
+  // Hide all carousel items
+  carouselItems3.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  // Show the slide at the specified index
+  carouselItems3[index].style.display = "block";
+}
+
+function nextSlide3() {
+  currentIndex3 = (currentIndex3 + 1) % carouselItems2.length;
+  showSlide3(currentIndex3);
+}
+
+function previousSlide3() {
+  currentIndex3 =
+    (currentIndex3 - 1 + carouselItems3.length) % carouselItems3.length;
+  showSlide3(currentIndex3);
+}
+
+// Show the first slide initially
+showSlide3(currentIndex3);
+
+// Set up event listeners for next and previous buttons
+document.getElementById("nextBtn3").addEventListener("click", nextSlide3);
+document.getElementById("prevBtn3").addEventListener("click", previousSlide3);
+
